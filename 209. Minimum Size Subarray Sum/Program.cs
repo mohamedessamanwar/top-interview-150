25e373f23dda7a9255e6c1ee99144b30cf4b0b24
mohamed essam <mohamed.essam.anwar2001@gmail.com>
2025-07-03 22:46:33 +0300

    Add "209. Minimum Size Subarray Sum" project
    Updated solution to include a new project for finding the minimum size subarray sum. Created project file with .NET SDK settings, modified Dockerfile for multi-stage builds, and added Program.cs with implementation logic. Updated launch settings for easier debugging and execution.

---
+++ 209. Minimum Size Subarray Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _209._Minimum_Size_Subarray_Sum
+{
+    internal class Program
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int sum = 0;
+            int l = 0;
+            int min = int.MaxValue;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                sum += nums[r];
+                while (sum >= target)
+                {
+                    min = Math.Min(min, r - l + 1);
+                    sum -= nums[l];
+                    l++;
+                }
+              //  min = Math.Min(min, r-l+1);
+            }
+            if(min == int.MaxValue)return 0;
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
