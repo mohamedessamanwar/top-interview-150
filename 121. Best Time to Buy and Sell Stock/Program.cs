09704e41b8e87af2356c9ebf53ad5e1e7d348153
mohamed essam <mohamed.essam.anwar2001@gmail.com>
2025-08-02 15:27:12 +0300

    Add Jump Game solution and update MaxProfit methods
    Implemented a new class for the "Jump Game" problem with a memoization approach in the `_55._Jump_Game` namespace. Created a new project file targeting .NET 9.0. Updated existing stock trading methods by adding a new `MaxProfitv2` method for profit calculation.

--- 121. Best Time to Buy and Sell Stock/Program.cs
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -2,7 +2,20 @@
 {
     internal class Program
     {
-        public int MaxProfit(int[] prices)
+        public int MaxProfitv2(int[] prices)
+        {
+            int sum = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    sum += prices[i] - prices[i - 1];
+                }
+            }
+
+            return sum;
+        }
+        public int MaxProfitV1(int[] prices)
         {
             if (prices == null || prices.Length == 0) return 0;
 
@@ -22,6 +35,37 @@
             }
 
             return maxProfit;
+        }
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int l = 0;
+            int r = 0;
+            if(nums2.Length == 0) return;
+            if (nums1.Length == 0) 
+            {
+                Array.Copy(nums2, nums1, n);
+                return;
+            }
+            for (int i = 0; i < m ; i++)
+            {
+                if (nums1[l] < nums2[r])
+                {
+                    nums1[i] = nums1[l];
+                    l++;
+                }
+                else
+                {
+                    int temp = nums1[l];
+                    nums1[l] = nums2[r];
+                    nums2[r] = temp;
+                    l++;
+                }
+            }
+            for (int i = m; i < n+m; i++)
+            {
+                nums1[i] = nums2[r];
+                r++;
+            }
         }
         static void Main(string[] args)
         {
---
+++ 55. Jump Game/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _55._Jump_Game
+{
+    internal class Program
+    {
+        public bool CanJump(int[] nums)
+        {
+            Dictionary<int, bool> memo = new Dictionary<int, bool>();
+            return CanJumpHelper(nums, 0, memo);
+        }
+
+        private bool CanJumpHelper(int[] nums, int index, Dictionary<int, bool> memo)
+        {
+            // Base case: reached or exceeded the last index
+            if (index >= nums.Length - 1)
+                return true;
+            
+            // Check memoization
+            if (memo.ContainsKey(index))
+                return memo[index];
+            
+            // Try all possible jumps from current position
+            for (int jump = 1; jump <= nums[index]; jump++)
+            {
+                if (CanJumpHelper(nums, index + jump, memo))
+                {
+                    memo[index] = true;
+                    return true;
+                }
+            }
+            
+            // No valid path found from this position
+            memo[index] = false;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
