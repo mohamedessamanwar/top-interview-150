3e0d581f7581ab3d38887920455ec881ef3c6ef2
mohamed essam <mohamed.essam.anwar2001@gmail.com>
2025-08-13 22:52:58 +0300

    Add project for Flatten Binary Tree to Linked List
    - Introduced a new project "114. Flatten Binary Tree to Linked List" in the solution file `top-interview-150.sln` with necessary configurations.
    - Created `114. Flatten Binary Tree to Linked List.csproj` specifying SDK, output type, target framework, and enabling implicit usings and nullable reference types.
    - Added `Program.cs` with a `TreeNode` class and a `Flatten` method to implement binary tree flattening logic, along with a simple `Main` method.
    
    Add project for Flatten Binary Tree to Linked List
    
    - Introduced a new project "114. Flatten Binary Tree to Linked List" in the solution file `top-interview-150.sln` with necessary configurations for Debug and Release modes.
    - Created `114. Flatten Binary Tree to Linked List.csproj` with project SDK, output type, target framework, and enabled features.
    - Added `Program.cs` containing a `TreeNode` class and a `Flatten` method to flatten a binary tree, along with a simple `Main` method.

---
+++ 114. Flatten Binary Tree to Linked List/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _114._Flatten_Binary_Tree_to_Linked_List
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public void Flatten(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            // Flatten the left subtree
+            Flatten(left);
+
+            if (left != null)
+            {
+
+                while (left.right != null)
+                {
+                    left = left.right;
+                }
+                // Connect the right subtree to the end of the left subtree
+                left.right = root.right;
+                root.right = root.left; // Set the right child of the root to the left subtree
+                root.left = null; // Set the left child of the left subtree to null
+            }
+            Flatten(right); // Flatten the right subtree
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
