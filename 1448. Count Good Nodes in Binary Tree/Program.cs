a86192bf5c1c236cc36b6b4844342c0fc32d10d9
mohamed essam <mohamed.essam.anwar2001@gmail.com>
2025-08-15 13:00:12 +0300

    Add project for "1448. Count Good Nodes in Binary Tree"
    - Introduced a new project in the solution file `top-interview-150.sln`.
    - Created project file `1448. Count Good Nodes in Binary Tree.csproj` with SDK, output type, target framework, and settings for implicit usings and nullable reference types.
    - Implemented a `Program` class in `Program.cs` containing a nested `TreeNode` class and a method `GoodNodes` to count good nodes in a binary tree. Added a `Main` method for application entry point.

---
+++ 1448. Count Good Nodes in Binary Tree/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _1448._Count_Good_Nodes_in_Binary_Tree
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x)
+            {
+                val = x;
+                left = null;
+                right = null;
+            }
+        }
+        public int GoodNodes(TreeNode root)
+        {
+            int Count = 0;
+            int Max = int.MinValue;
+            GoodNodsHelper(Max, ref Count, root);
+
+            void GoodNodsHelper(int max, ref int count, TreeNode treeNode)
+            {
+                if (treeNode == null) return;
+                if (treeNode.val > max)
+                {
+                    count++;
+                    max = treeNode.val;
+                }
+                GoodNodsHelper(max, ref count, treeNode.left);
+                GoodNodsHelper(max, ref count, treeNode.right);
+            }
+            return Count;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
